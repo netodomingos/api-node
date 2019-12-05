85467b6c234061ba8f95cc9aa6229c6ac7b8eaaf
Geraldo Domingos <geraldo.dsn2000@gmail.com>
2019-12-05 12:55:15 -0300

    Finalização do cadastro
    - Cadastro funcionando.
    - Password Incriptada.
    - Mensagens de Bad Request feitas.

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -8,9 +8,19 @@ const router = express.Router()
 // Rota de Cadastro do usuário
 router.post('/register', async (request, response) => {
 
+    const { email } = request.body
+
     // tentar adicionar as informações que o usúario passar, na database.
     try{
+        //Checar se o Email cadastrado já existe
+        if(await User.findOne({email}))
+            return response.status(400).send({ error: 'User Already Exists' })
+        
+        // Criando o usuario
         const user = await User.create(request.body)
+
+        // Fazendo o password deixar de aparecer
+        user.password = undefined
 
         return response.send({ user })
     }
--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('../database/index')
+const bcrypt = require('bcryptjs')
 
 // Criando Schema do usuário
 const UserSchema = new mongoose.Schema({
@@ -22,6 +23,16 @@ const UserSchema = new mongoose.Schema({
         type: Date,
         default: Date.now, // Retorna a data atual como default.
     },
+})
+// Configurações antes de salvar o user
+UserSchema.pre('save', async function (next){
+    // Criando Hash e fazendo ele apontar para a password
+    const hash = await bcrypt.hash(this.password, 10)
+
+    // Passando o valor de Hash para ser apresentando no password
+    this.password = hash
+
+    next()
 })
 
 // Colocando tudo de UserSchema em User
